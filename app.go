a57bc250e291284a1d5d4c092b3d4f0c8fd85520
Xuan Tang <tangbixuan@gmail.com>
2016-08-30 14:37:49 -0700

    able to create function from upload.html

--- app.go
+++ app.go
@@ -11,14 +11,53 @@ import (
 )
 
 var (
-	d = *Docker
+	defaultDockerHost    = "unix:///var/run/docker.sock"
+	defaultDockerVersion = "v1.22"
+	defaultDockerHeaders = map[string]string{"User-Agent": "engine-api-cli-1.0"}
 )
 
-func init() {
-	cfg := &DockerConfig{
-		HttpHeaders: docker.defaultDockerHeaders,
-		Host:        docker.defaultDockerHost,
-		Version:     docker.defaultDockerVersion,
+/*
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+	uploadFile(w, r)
+	d.BuildFunction("xuant", "testfunc", "python27")
+}
+*/
+func uploadFile(w http.ResponseWriter, r *http.Request) error {
+
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return err
+	}
+
+	defer file.Close()
+
+	out, err := os.Create(docker.IBContext + docker.ExecutionFile)
+
+	if err != nil {
+		fmt.Fprintf(w, "Permission denied. Unable to create file "+docker.IBContext+docker.ExecutionFile)
+		return err
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return err
+	}
+
+	fmt.Fprintf(w, "File uploaded successfully: %s", header.Filename)
+	return nil
+}
+
+func main() {
+	cfg := &docker.DockerConfig{
+		HttpHeaders: defaultDockerHeaders,
+		Host:        defaultDockerHost,
+		Version:     defaultDockerVersion,
 		HttpClient:  nil,
 	}
 
@@ -26,35 +65,18 @@ func init() {
 	if err != nil {
 		log.Printf("Failed to NewDocker. Error: %s", err)
 	}
-}
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		fmt.Fprintln(w, err)
-		return
-	}
-
-	defer file.Close()
-
-	out, err := os.Create(docker.ibContext + docker.executionFile)
-	if err != nil {
-		fmt.Fprintf(w, "Permission denied. Unable to create file "+docker.ibContext+docker.executionFile)
-		return
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, file)
-	if err != nil {
-		fmt.Fprintln(w, err)
-	}
-
-	fmt.Fprintf(w, "File uploaded successfully : %s", header.Filename)
-}
-
-func main() {
-	http.HandleFunc("/receive", uploadHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/receive", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		if err := uploadFile(w, r); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("File uploaded")
+		if err := d.BuildFunction("xuant", "testfunc", "python27"); err != nil {
+			log.Println(err)
+			return
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- docker/docker.go
+++ docker/docker.go
@@ -1,42 +1,33 @@
-package kexec
+package docker
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"golang.org/x/net/context"
+
+	"github.com/docker/docker/builder"
+	"github.com/docker/docker/builder/dockerignore"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/fileutils"
+	"github.com/docker/docker/pkg/progress"
+	"github.com/docker/docker/pkg/streamformatter"
 	"github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
-	"golang.org/x/net/context"
 )
 
 var (
-	ibContext     = "/tmp/faas-imagebuild-context/"
-	executionFile = "exec"
-
-	defaultDockerHost    = "unix:///var/run/docker.sock"
-	defaultDockerVersion = "v1.22"
-	defaultDockerHeaders = map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	IBContext     = "/tmp/faas-imagebuild-context/"
+	RelDockerfile = "Dockerfile"
+	ExecutionFile = "exec"
 )
-
-func main() {
-	cli, err := client.NewClient(defaultHost, defaultVersion, defaultHttpClient, defaultHeaders)
-	if err != nil {
-		panic(err)
-	}
-
-	options := types.ContainerListOptions{All: true}
-	containers, err := cli.ContainerList(context.Background(), options)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, c := range containers {
-		fmt.Println(c.ID, c.Names, c.Image)
-	}
-}
 
 type DockerConfig struct {
 	HttpHeaders map[string]string
@@ -56,36 +47,72 @@ func NewDocker(c *DockerConfig) (*Docker, error) {
 	}
 	return &Docker{
 		client: cli,
-	}
+	}, nil
 }
 
 func (d *Docker) BuildFunction(namespace, funcName, templateName string) error {
-	err := setRuntimeTemplate(templateName)
+	if _, err := os.Stat(IBContext + ExecutionFile); err != nil {
+		log.Printf("Failed build function. Error: Execution file not found.")
+		return errors.New("Execution file not found.")
+	}
 
-	if err != nil {
-		log.Printf("Failed to set up runtime template. Error: %s", err)
+	if err := setRuntimeTemplate(templateName); err != nil {
+		log.Printf("Failed to set up runtime template. Error:%s", err)
 		return err
 	}
 
-	ctx, err := os.Open(ibContext)
+	f, err := os.Open(filepath.Join(IBContext, ".dockerignore"))
 
-	if err != nil {
-		log.Printf("Failed to open image build context %s. Error: %s", ibContext, err)
+	if err != nil && !os.IsNotExist(err) {
 		return err
 	}
+	defer f.Close()
+
+	var excludes []string
+	if err == nil {
+		excludes, err = dockerignore.ReadAll(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := builder.ValidateContextDirectory(IBContext, excludes); err != nil {
+		return fmt.Errorf("Error checking context: '%s'.", err)
+	}
+
+	var includes = []string{"."}
+	keepThem1, _ := fileutils.Matches(".dockerignore", excludes)
+	keepThem2, _ := fileutils.Matches(RelDockerfile, excludes)
+	if keepThem1 || keepThem2 {
+		includes = append(includes, ".dockerignore", RelDockerfile)
+	}
+
+	buildCtx, err := archive.TarWithOptions(IBContext, &archive.TarOptions{
+		Compression:     archive.Uncompressed,
+		ExcludePatterns: excludes,
+		IncludeFiles:    includes,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	progressOutput := streamformatter.NewStreamFormatter().NewProgressOutput(bytes.NewBuffer(nil), true)
+	var body io.Reader = progress.NewProgressReader(buildCtx, progressOutput, 0, "", "Sending build context to Docker daemon")
 
 	opts := types.ImageBuildOptions{
-		Tags:   []string{"faas:v0.1"},
-		Squash: true,
+		Tags:       []string{namespace + "/" + funcName},
+		Dockerfile: RelDockerfile,
+		Squash:     true,
 	}
 
-	resp, err := d.client.ImageBuild(context.Background(), ctx, opts)
-	defer resp.Body.Close()
+	resp, err := d.client.ImageBuild(context.Background(), body, opts)
 
 	if err != nil {
 		log.Printf("Failed to build image. Error: %s", err)
 		return err
 	}
+	defer resp.Body.Close()
 
 	buf, err := ioutil.ReadAll(resp.Body)
 
@@ -108,14 +135,14 @@ ENTRYPOINT [ "python", "exec" ]
 // setRuntimeEnv creates the runtime environment for building a docker image.
 //
 // Based on the templateName, this method will create a corresponding Dockerfile
-// in ibContext (i.e. /tmp/faas-imagebuild-context). To make the build process fast,
+// in IBContext (i.e. /tmp/faas-imagebuild-context). To make the build process fast,
 // runtime template should be proloaded onto the system.
 //
 // Now supporting Python27 only. Other template can be added easi
 func setRuntimeTemplate(templateName string) error {
 	switch templateName {
 	case "python27":
-		ioutil.WriteFile(ibContext+"Dockerfile", []byte(python27Template), 0644)
+		ioutil.WriteFile(IBContext+"Dockerfile", []byte(python27Template), 0644)
 		return nil
 	default:
 		return errors.New("Runtime template " + templateName + " invalid or not supported yet.")
