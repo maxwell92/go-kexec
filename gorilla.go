1455a73a2b2fda476868ae0d6dcca9828869e361
renjingj <renjingj@gmail.com>
2016-10-19 03:56:09 +0000

    Use customized http handler

--- gorilla.go
+++ gorilla.go
@@ -21,19 +21,80 @@ import (
 var (
 	// default docker registry
 	defaultDockerRegistry = "registry.paas.symcpe.com:443"
+)
 
-	// gorilla web http router
-	router = mux.NewRouter()
+// Error represents a handler error. It provides methods for a HTTP status
+// code and embeds the built-in error interface.
+type Error interface {
+	error
+	Status() int
+}
 
+// StatusError represents an error with an associated HTTP status code.
+type StatusError struct {
+	Code int
+	Err  error
+}
+
+// Allows StatusError to satisfy the error interface.
+func (se StatusError) Error() string {
+	return se.Err.Error()
+}
+
+// Returns our HTTP status code.
+func (se StatusError) Status() int {
+	return se.Code
+}
+
+type appConfig struct {
+	dockerRegistry string
+	ldapcfg        *ldapConfig
+}
+type ldapConfig struct {
+	ldapServer  []string
+	ldapPort    int
+	ldapRetries int
+	ldapBaseDn  string
+}
+type appContext struct {
+	d             *docker.Docker
+	k             *kexec.Kexec
+	cookieHandler *securecookie.SecureCookie
+	conf          *appConfig
+}
+type appHandler struct {
+	*appContext
+	H func(*appContext, http.ResponseWriter, *http.Request) error
+}
+
+func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := ah.H(ah.appContext, w, r)
+	if err != nil {
+		switch e := err.(type) {
+		case Error:
+			// We can retrieve the status here and write out a specific
+			// HTTP status code.
+			log.Printf("HTTP %d - %s", e.Status(), e)
+			http.Error(w, e.Error(), e.Status())
+		default:
+			// Any error types we don't specifically look out for default
+			// to serving a HTTP 500
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+		}
+	}
+}
+
+func main() {
 	// cookie handling
-	cookieHandler = securecookie.New(
+	cookieHandler := securecookie.New(
 		securecookie.GenerateRandomKey(64),
 		securecookie.GenerateRandomKey(32),
 	)
 
 	// docker handler for creating function and pushing function image
 	// to docker registry
-	d = docker.NewDocker(
+	d := docker.NewDocker(
 		// http headers
 		map[string]string{"User-Agent": "engin-api-cli-1.0"},
 		// docker host
@@ -46,32 +107,32 @@ var (
 
 	// kubernetes handler for calling function and pulling function
 	// execution logs
-	k, _ = kexec.NewKexec(&kexec.KexecConfig{
+	k, _ := kexec.NewKexec(&kexec.KexecConfig{
 		KubeConfig: os.Getenv("HOME") + "/.kube/config",
 	})
-)
 
-func main() {
-
+	context := &appContext{d: d, k: k, cookieHandler: cookieHandler, conf: nil}
+	// gorilla web http router
+	router := mux.NewRouter()
 	// IndexPageHandler handles index page (i.e. login page)
-	router.HandleFunc("/", IndexPageHandler)
+	router.Handle("/", appHandler{context, IndexPageHandler})
 
 	// LoginHandler create session from login page information,
 	// do basic authentication, and redirect to the internal
 	// control panel if authenticated.
 	//
 	// LogoutHandler clears session and redirect to index page.
-	router.HandleFunc("/login", LoginHandler).Methods("POST")
-	router.HandleFunc("/logout", LogoutHandler)
+	router.Handle("/login", appHandler{context, LoginHandler}).Methods("POST")
+	router.Handle("/logout", appHandler{context, LogoutHandler})
 
 	// InternalPageHandler displays the internal control panel
-	router.HandleFunc("/internal", InternalPageHandler)
+	router.Handle("/internal", appHandler{context, InternalPageHandler})
 
 	// CreateFunctionHandler handles `create function` request
-	router.HandleFunc("/create", CreateFunctionHandler)
+	router.Handle("/create", appHandler{context, CreateFunctionHandler})
 
 	// CallFunctionHandler handles `call function` request
-	router.HandleFunc("/call", CallFunctionHandler)
+	router.Handle("/call", appHandler{context, CallFunctionHandler})
 
 	/* ... Static files not used currently
 	staticServer := http.StripPrefix("/ui/", http.FileServer(http.Dir("./ui")))
@@ -82,9 +143,9 @@ func main() {
 	panic(http.ListenAndServe(":8080", nil))
 }
 
-func CallFunctionHandler(response http.ResponseWriter, request *http.Request) {
+func CallFunctionHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
 
-	userName := getUserName(request)
+	userName := getUserName(a, request)
 	functionName := getFunctionName(request)
 
 	if userName == "" || functionName == "" {
@@ -103,10 +164,9 @@ func CallFunctionHandler(response http.ResponseWriter, request *http.Request) {
 
 			// Log on server side and notify client
 			log.Printf("Failed to create uuid for function call. Error: %s", err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
 
 			// Return immediately when there is an error
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		uuidStr := uuid.String() // uuidStr needed when fetching log
@@ -115,24 +175,24 @@ func CallFunctionHandler(response http.ResponseWriter, request *http.Request) {
 		image := defaultDockerRegistry + "/" + userName + "/" + functionName
 		labels := make(map[string]string)
 
-		if err = k.CallFunction(jobname, image, userName, labels); err != nil {
+		if err = a.k.CallFunction(jobname, image, userName, labels); err != nil {
 
 			log.Printf("Failed to call function %s. Error: %s", functionName, err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
 
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		fmt.Fprintf(response, html.FunctionCalledPage)
 	}
+	return nil
 }
 
 func getFunctionName(request *http.Request) string {
 	return "Not implemented yet."
 }
 
-func CreateFunctionHandler(response http.ResponseWriter, request *http.Request) {
-	userName := getUserName(request)
+func CreateFunctionHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
+	userName := getUserName(a, request)
 	if userName == "" {
 
 		// Empty username is not allowed to create function
@@ -156,8 +216,7 @@ func CreateFunctionHandler(response http.ResponseWriter, request *http.Request) 
 		if functionName == "" || runtime == "" || code == "" {
 			err := errors.New("Something's wrong with FunctionName/Runtime/Code.")
 			log.Printf("Function failed: something's wrong with Function Name/Runtime/Code.")
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		log.Printf("Code uploaded:\n%s", code)
@@ -169,49 +228,47 @@ func CreateFunctionHandler(response http.ResponseWriter, request *http.Request) 
 
 		if err != nil {
 			log.Printf("Function failed: %s", err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 		defer exeFile.Close()
 
 		// Write the function into the execution file
 		if _, err = exeFile.WriteString(code); err != nil {
 			log.Printf("Function failed: %s", err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		// Build funtion
-		if err = d.BuildFunction(defaultDockerRegistry, userName, functionName, runtime); err != nil {
+		if err = a.d.BuildFunction(defaultDockerRegistry, userName, functionName, runtime); err != nil {
 			log.Printf("Build function failed: %s", err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		// Register function to configured docker registry
 		if err = docker.RegisterFunction(defaultDockerRegistry, userName, functionName); err != nil {
 			log.Printf("Register function failed: %s", err)
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
+			return StatusError{http.StatusInternalServerError, err}
 		}
 
 		// If all the above operation succeeded, the function is created
 		// successfully.
 		fmt.Fprintf(response, html.FunctionCreatedPage)
 	}
+	return nil
 }
 
-func IndexPageHandler(response http.ResponseWriter, request *http.Request) {
-	userName := getUserName(request)
+func IndexPageHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
+	userName := getUserName(a, request)
 	if userName != "" {
 		//Already logged in, show internal page
 		fmt.Fprintf(response, html.InternalPage, userName)
 	} else {
 		fmt.Fprintf(response, html.IndexPage)
 	}
+	return nil
 }
 
-func LoginHandler(response http.ResponseWriter, request *http.Request) {
+func LoginHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
 	name := request.FormValue("name")
 	pass := request.FormValue("password")
 	redirectTarget := "/"
@@ -236,34 +293,37 @@ func LoginHandler(response http.ResponseWriter, request *http.Request) {
 				"<input type=\"password\" id=\"password\" name=\"password\">"+
 				"<button type=\"submit\">Login</button>"+
 				"</form>", errMsg)
-			return
+			return nil
 		}
-		setSession(name, response)
+		setSession(a, name, response)
 		redirectTarget = "/internal"
 	}
 	http.Redirect(response, request, redirectTarget, http.StatusFound)
+	return nil
 }
 
-func InternalPageHandler(response http.ResponseWriter, request *http.Request) {
-	userName := getUserName(request)
+func InternalPageHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
+	userName := getUserName(a, request)
 	if userName != "" {
 		fmt.Fprintf(response, html.InternalPage, userName)
 	} else {
 		http.Redirect(response, request, "/", http.StatusFound)
 	}
+	return nil
 }
 
-func LogoutHandler(response http.ResponseWriter, request *http.Request) {
+func LogoutHandler(a *appContext, response http.ResponseWriter, request *http.Request) error {
 	clearSession(response)
 	log.Println("Logged out")
 	http.Redirect(response, request, "/", http.StatusFound)
+	return nil
 }
 
-func setSession(userName string, response http.ResponseWriter) {
+func setSession(a *appContext, userName string, response http.ResponseWriter) {
 	value := map[string]string{
 		"name": userName,
 	}
-	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+	if encoded, err := a.cookieHandler.Encode("session", value); err == nil {
 		cookie := &http.Cookie{
 			Name:  "session",
 			Value: encoded,
@@ -283,10 +343,10 @@ func clearSession(response http.ResponseWriter) {
 	http.SetCookie(response, cookie)
 }
 
-func getUserName(request *http.Request) (userName string) {
+func getUserName(a *appContext, request *http.Request) (userName string) {
 	if cookie, err := request.Cookie("session"); err == nil {
 		cookieValue := make(map[string]string)
-		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+		if err = a.cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
 			userName = cookieValue["name"]
 		}
 	}
